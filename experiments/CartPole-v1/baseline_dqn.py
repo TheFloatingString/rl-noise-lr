5f5927fa0668c5d6ab7ecd0207f414d5ae1fc3d8
Laurence Liang <laurence.w.liang@gmail.com>
2024-01-24 01:44:32 -0500

    Add DQN baseline

---
+++ experiments/CartPole-v1/baseline_dqn.py
@@ -0,0 +1,34 @@
+import gymnasium as gym
+from stable_baselines3 import A2C, DQN, PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.logger import configure
+from src.environments import NoiseWrapper 
+from typing import Callable
+config = {
+    "policy_type": "MlpPolicy",
+    "total_timesteps": -1,
+    "env_name": "CartPole-v1"
+}
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return 0.5*(progress_remaining * initial_value)+0.5*initial_value
+    return func
+tmp_path = "/tmp/sb3_log/"
+# env = NoiseWrapper(gym.make("CartPole-v1", render_mode
+# =None), noise_param=0, shape=(4,))
+env = gym.make("CartPole-v1", render_mode=None)
+model = DQN("MlpPolicy", env=env, verbose=1, learning_rate=0.0001, tensorboard_log='./tmp_tensorboard')
+for i in range(1):
+    model.learn(total_timesteps=2000000, log_interval=1, progress_bar=True)
